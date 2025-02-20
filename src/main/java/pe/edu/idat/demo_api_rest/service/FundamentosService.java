c4ede7f0e2da402ec348f16ac0d07e9dcb6fc8f5
Sk-gabo <gabriel123ceras12@gmail.com>
2025-02-20 10:03:20 -0500

    Algoritmos del servicio finalizado

--- src/main/java/pe/edu/idat/demo_api_rest/service/FundamentosService.java
+++ src/main/java/pe/edu/idat/demo_api_rest/service/FundamentosService.java
@@ -1,4 +1,51 @@
 package pe.edu.idat.demo_api_rest.service;
 
-public class FundamentosService {
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FundamentosService  implements  IFundamentosService{
+
+    @Override
+    public String validarNumeroPrimo(int numero) {
+        if(numero < 2){
+            return "No es PRIMO";
+        }
+        for(int i = 0; i <= Math.sqrt(numero); i++){
+            if(numero % i == 0){
+                return "No es PRIMO";
+            }
+        }
+        return "Es PRIMO";
+    }
+
+    @Override
+    public String calcularFactorial(int numero) {
+        if(numero < 0){
+            return "No se puede calcular factorial";
+        }
+        long factorial = 1;
+        for(int i = 1; i <= numero; i++){
+            factorial += i;
+        }
+        return "El factorial es = " + factorial;
+    }
+
+    @Override
+    public List<Integer> calcularFibonacci(int numero) {
+        List<Integer> fibonacci = new ArrayList<>();
+        if(numero <= 0){
+            return fibonacci;
+        }
+        int a = 0, b= 1;
+        for(int i = 0; i < numero; i++){
+            fibonacci.add(a);
+            int siguienteValor = a + b;
+            a = b;
+            b = siguienteValor;
+        }
+        return fibonacci;
+    }
 }
--- src/main/java/pe/edu/idat/demo_api_rest/service/IFundamentosService.java
+++ src/main/java/pe/edu/idat/demo_api_rest/service/IFundamentosService.java
@@ -1,4 +1,16 @@
 package pe.edu.idat.demo_api_rest.service;
 
+import java.util.List;
+
 public interface IFundamentosService {
+    //Solo es metodos
+
+    String validarNumeroPrimo(int numero);
+
+    String calcularFactorial(int numero);
+
+    List<Integer> calcularFibonacci(int numero);
+
+
+
 }
